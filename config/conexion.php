59bc32f71ca9201ce1cc3436915768c702e43933
Zerik209 <zerikoficial@gmail.com>
2023-05-21 05:27:14 -0600

    prueba5

--- config/conexion.php
+++ config/conexion.php
@@ -1,16 +1,11 @@
 <?php
-$host = "shop.mysql.database.azure.com";
-$username = "Zerik209@shop";
-$password = "Basquetball209";
-$database = "card";
-
-$conexion = mysqli_init();
-$conexion->ssl_set(NULL, NULL, NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL);
-$conexion->real_connect($host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL);
+$con = mysqli_init();
+mysqli_ssl_set($con, NULL, NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($con, "shop.mysql.database.azure.com", "Zerik209@shop", "Basquetball209", "card", 3306, NULL, MYSQLI_CLIENT_SSL);
 
 // Verificar si hay errores de conexión
-if ($conexion->connect_errno) {
-    die("Error de conexión confirmado4: " . $conexion->connect_error);
+if (mysqli_connect_errno()) {
+    die("Error de conexión5: " . mysqli_connect_error());
 }
 
 ?>
