f4c98032feaf122ce5471283651808d15fc7e951
Zerik209 <zerikoficial@gmail.com>
2023-05-21 05:18:24 -0600

    prueba4

--- config/conexion.php
+++ config/conexion.php
@@ -1,6 +1,6 @@
 <?php
 $host = "shop.mysql.database.azure.com";
-$username = "Zerik209";
+$username = "Zerik209@shop";
 $password = "Basquetball209";
 $database = "card";
 
@@ -10,23 +10,7 @@ $conexion->real_connect($host, $username, $password, $database, 3306, NULL, MYSQ
 
 // Verificar si hay errores de conexión
 if ($conexion->connect_errno) {
-    die("Error de conexión confirmado2: " . $conexion->connect_error);
+    die("Error de conexión confirmado4: " . $conexion->connect_error);
 }
 
-$query = mysqli_query($conexion, "SELECT * FROM categorias");
-
-// Verificar si hay errores en la ejecución de la consulta
-if (!$query) {
-    echo "Error en la consulta: " . mysqli_error($conexion);
-    // Otra lógica de manejo de errores si es necesario
-} else {
-    // Continuar con el procesamiento de los resultados
-    while ($row = mysqli_fetch_assoc($query)) {
-        // Procesar los datos obtenidos
-        // Ejemplo: echo $row['columna'];
-    }
-}
-
-// Cerrar la conexión
-mysqli_close($conexion);
 ?>
