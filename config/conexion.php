e5fbb18c6e1c6af2135f523a9d6096c87d975262
Zerik209 <zerikoficial@gmail.com>
2023-05-21 05:57:55 -0600

    prueba7

--- config/conexion.php
+++ config/conexion.php
@@ -2,10 +2,10 @@
 $conexion = mysqli_init();
 mysqli_ssl_set($conexion, NULL, NULL, "ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 
-mysqli_real_connect($conexion, "shop.mysql.database.azure.com", "Zerik209", "Basquetball209", "card", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conexion, "shop.mysql.database.azure.com", "Zerik209@shop", "Basquetball209", "card", 3306, MYSQLI_CLIENT_SSL);
 
 if (mysqli_connect_errno()) {
-    die("Error de conexión a la base de datos6: " . mysqli_connect_error());
+    die("Error de conexión a la base de datos7: " . mysqli_connect_error());
 } else {
     echo "Conexión exitosa a la base de datos.";
 }
