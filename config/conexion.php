6723218a38a030849129f709edbe3f850fd2f607
Zerik209 <zerikoficial@gmail.com>
2023-05-21 05:29:36 -0600

    prueba6

--- config/conexion.php
+++ config/conexion.php
@@ -1,11 +1,11 @@
 <?php
-$con = mysqli_init();
+$conexion = mysqli_init();
 mysqli_ssl_set($con, NULL, NULL, "{path to CA cert}", NULL, NULL);
 mysqli_real_connect($con, "shop.mysql.database.azure.com", "Zerik209@shop", "Basquetball209", "card", 3306, NULL, MYSQLI_CLIENT_SSL);
 
 // Verificar si hay errores de conexión
 if (mysqli_connect_errno()) {
-    die("Error de conexión5: " . mysqli_connect_error());
+    die("Error de conexión6: " . mysqli_connect_error());
 }
 
 ?>
